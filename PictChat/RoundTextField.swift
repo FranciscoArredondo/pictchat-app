8306bcda119a702d9b6b4a412db15aff15b1afc1
frarredo <frarredo@gmail.com>
2017-07-20 23:16:37 -0700

    completed login UI

---
+++ PictChat/RoundTextField.swift
@@ -0,0 +1,47 @@
+//
+//  RoundTextField.swift
+//  PictChat
+//
+//  Created by Francisco on 7/20/17.
+//
+//
+
+import UIKit
+
+@IBDesignable
+class RoundTextField: UITextField {
+
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        didSet {
+            layer.borderColor = borderColor?.cgColor
+        }
+    }
+    
+    @IBInspectable var bgColor: UIColor? {
+        didSet {
+            backgroundColor = bgColor
+        }
+    }
+    
+    @IBInspectable var placeholderColor: UIColor? {
+        didSet {
+            let rawString = attributedPlaceholder?.string != nil ? attributedPlaceholder!.string : ""
+            let str = NSAttributedString(string: rawString, attributes: [NSForegroundColorAttributeName: placeholderColor!])
+            attributedPlaceholder = str
+        }
+    }
+
+}
---
+++ PictChat/RoundedButton.swift
@@ -0,0 +1,39 @@
+//
+//  RoundedButton.swift
+//  PictChat
+//
+//  Created by Francisco on 7/20/17.
+//
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        didSet {
+            layer.borderColor = borderColor?.cgColor
+        }
+    }
+    
+    @IBInspectable var bgColor: UIColor? {
+        didSet {
+            backgroundColor = bgColor
+        }
+    }
+
+}
